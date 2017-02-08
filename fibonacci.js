4189933da9e1aabfcffd17472ad0de6032f9ec22
jainvishal12 <jainvishal12@bitbucket.org>
2017-02-08 16:33:32 +0530

    Fibonacci-series algorithm

---
+++ fibonacci.js
@@ -0,0 +1,34 @@
+//Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+//*************** For loop solution ***********//
+function fibonacci(num){
+var a=1,b=0,temp;
+while(num>=0){
+  temp = a;
+  a = a+b;
+  b=temp;
+  num--
+}
+return b;
+}
+
+
+//*************** While loop solution ***********//
+function fibonacci(num){
+  var a=1,b=0,temp;
+  for(var i = 0;i<=num;i++){
+    temp = a;
+    a= a+b;
+    b =temp;
+  }
+  return b;
+}
+
+
+//*************** Recursive solution ***********//
+function fibonacci(num){
+return (num<=1)?1:fibonacci(num-1)+fibonacci(num-2);
+}
+
+
+fibonacci(6)//1,1,2,3,5,8,13......
