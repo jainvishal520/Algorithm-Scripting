afd8671ba8b81e06027128a28e3a20734424acae
Vishal <vjtinu.jain431@gmail.com>
2017-02-14 20:33:44 +0530

    seek and destroy

--- steam-roller.js
+++ steam-roller.js
@@ -1,29 +1,16 @@
-// Flatten a nested array. You must account for varying levels of nesting.
+// You will be provided with an initial array (the first argument in the destroyer function), 
+// followed by one or more arguments.
+// Remove all elements from the initial array that are of the same value as these arguments.
 
 
 
-
-function steamrollArray(arr) {
-
-//   var flattenedArr = [];
-//   var flatten = function(arg){
-//     if(Array.isArray(arg)){
-//      for(var a in arg){
-//         flatten(arg[a]);
-//       }
-//     }else{
-//       flattenedArr.push(arg);
-//     }
-//   };
-//   arr.forEach(flatten);
-  
-//   return flattenedArr;
-  
-  
-  return arr.reduce(function(x,y){
-    return x.concat(Array.isArray(y)?steamrollArray(y) : y);
-  },[]);
+function destroyer(arr) {
+  // Remove all the values
+  var compArr = Array.prototype.slice.call(arguments,1);
+  return arr.filter(function(x){
+    return compArr.indexOf(x) === -1;
+  });
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
